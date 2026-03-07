76408cf2821ff63d4f79fc7d45a2eb07ca504a5d
Dark99965 <noah.retter@icloud.com>
2026-03-07 14:46:48 -0400

    sry i dint do any commits but i fished this project

--- src/main.js
+++ src/main.js
@@ -1,61 +1,134 @@
 /** @type {HTMLCanvasElement} */
+
 // intalse
+
 let canvas = document.querySelector("#Game");
+
 let game = canvas.getContext("2d");
-game.fillStyle = "Red";
+
 // vars for player
 
 let player = {
-    posx: 0,
-    posy: 0
-}
+  posx: 0,
+  posy: 0,
+};
 
-let coins = []
+let coin = {
+  x: 500,
+  y: 500,
+};
+
+//  scroe
+
+let score = 0;
+
+//  keys
+
+let keys = [];
 
 // Random func
 
 function randomr(min, max) {
-    let cmin = Math.ceil(min)
-    let fmax = Math.floor(max)
-    return Math.floor(Math.random() * (fmax - cmin) + cmin)
+  let cmin = Math.ceil(min);
+
+  let fmax = Math.floor(max);
+
+  return Math.floor(Math.random() * (fmax - cmin) + cmin);
 }
 
-game.fillRect(player.posx, player.posy, 50, 50)
+// movement
 
-// MAIN
 window.addEventListener("keydown", (e) => {
-    // Movement
-    if (e.key === "w") {
-        player.posy -= 5
-    }
+  if (e.key === "w") {
+    keys["w"] = true;
+  }
 
-    if (e.key === "s") {
-        player.posy += 5
-    }
+  if (e.key === "s") {
+    keys["s"] = true;
+  }
 
-    if (e.key === "d") {
-        player.posx += 5
-    }
+  if (e.key === "d") {
+    keys["d"] = true;
+  }
 
-    if (e.key === "a") {
-        player.posx -= 5
-    }
+  if (e.key === "a") {
+    keys["a"] = true;
+  }
+});
 
-    if (e.key === " ") {
-        if (coins[0]) {
-            coins.splice(0, 1)
-        }
-        coins.push({
-            x: randomr(0, canvas.width),
-            y: randomr(0, canvas.height)
-        })
-    }
+window.addEventListener("keyup", (e) => {
+  if (e.key === "w") {
+    keys["w"] = false;
+  }
 
-    game.clearRect(0, 0, canvas.width, canvas.height)
-    game.strokeStyle = "blue"
-    coins.forEach(coin => {
-        game.strokeRect(coin.x, coin.y, 50, 50)
-    })
-    game.fillRect(player.posx, player.posy, 50, 50)
+  if (e.key === "s") {
+    keys["s"] = false;
+  }
 
-})
+  if (e.key === "d") {
+    keys["d"] = false;
+  }
+
+  if (e.key === "a") {
+    keys["a"] = false;
+  }
+});
+
+// starting stuff
+
+game.fillStyle = "Red";
+
+game.fillRect(player.posx, player.posy, 50, 50);
+
+game.font = "30px sans-serif";
+
+coin.x = randomr(0, canvas.width - 50);
+
+coin.y = randomr(0, canvas.height - 50);
+
+game.fillStyle = "blue";
+
+game.fillRect(coin.x, coin.y, 50, 50);
+
+// * GAME LOOP
+
+function gameloop() {
+  requestAnimationFrame(gameloop);
+
+  if (keys.w) player.posy -= 5;
+
+  if (keys.s) player.posy += 5;
+
+  if (keys.d) player.posx += 5;
+
+  if (keys.a) player.posx -= 5;
+
+  if (
+    player.posx < coin.x + 50 &&
+    player.posx + 50 > coin.x &&
+    player.posy < coin.y + 50 &&
+    player.posy + 50 > coin.y
+  ) {
+    score++;
+
+    coin.x = randomr(0, canvas.width - 50);
+
+    coin.y = randomr(0, canvas.height - 50);
+  }
+
+  game.clearRect(0, 0, canvas.width, canvas.height);
+
+  game.fillStyle = "black";
+
+  game.fillText(String(score), 250, 50);
+
+  game.fillStyle = "blue";
+
+  game.fillRect(coin.x, coin.y, 50, 50);
+
+  game.fillStyle = "Red";
+
+  game.fillRect(player.posx, player.posy, 50, 50);
+}
+
+gameloop();
